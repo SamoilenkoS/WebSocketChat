e3f3211c19b6f124f70165b9279b941c3bfd880d
Sviatoslav Samoilenko <sviatoslavs@wizardsdev.com>
2021-07-07 17:38:30 +0300

    Add project files.

---
+++ WebSocketChat/SocketManager/SocketHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketChat.SocketManager
+{
+    public class SocketHandler
+    {
+        public ConnectionManager Connections { get; set; }
+
+        public SocketHandler(ConnectionManager connections)
+        {
+            Connections = connections;
+        }
+
+        public virtual async Task OnConnected(WebSocket socket)
+        {
+            await Task.Run(() => { Connections.AddSocket(socket); });
+        }
+
+        public virtual async Task OnDisconnected(WebSocket socket)
+        {
+            await Connections.RemoveSocketAsync(Connections.GetId(socket));
+        }
+
+        public async Task SendMessage(WebSocket socket, string message)
+        {
+            if (socket.State != WebSocketState.Open)
+            {
+                return;
+            }
+
+            await socket.SendAsync(
+                new ArraySegment<byte>(Encoding.ASCII.GetBytes(message), 0, message.Length),
+                WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+    }
+}
